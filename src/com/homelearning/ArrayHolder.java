67988ead75966ae2146d7b0c973a32d12c49a1aa
Vitaliy Ostapenko <soul33d@gmail.com>
2017-04-15 00:17:21 +0300

    Initital commit of the project

---
+++ src/com/homelearning/ArrayHolder.java
@@ -0,0 +1,39 @@
+package com.homelearning;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayHolder {
+
+    private int[] initialArray = new int[5];
+    private int[] sortedArray;
+
+    void execute(){
+        readArrayFromInput();
+        sortedArray = initialArray.clone();
+        printArray("Initial initialArray:", initialArray);
+        sortArray();
+        printArray("Sorted initialArray:", sortedArray);
+    }
+
+    private void readArrayFromInput() {
+        for (int i = 0; i < initialArray.length; i++) {
+            System.out.println(String.format("Enter number %d:", i + 1));
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()){
+                initialArray[i] = scanner.nextInt();
+            } else {
+                System.out.println("Invalid input, please enter a number");
+                i--;
+            }
+        }
+    }
+
+    private void printArray(String msg, int[] array){
+        System.out.println(msg + Arrays.toString(array));
+    }
+
+    private void sortArray(){
+        Arrays.sort(sortedArray);
+    }
+}
---
+++ src/com/homelearning/Main.java
@@ -0,0 +1,8 @@
+package com.homelearning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new ArrayHolder().execute();
+    }
+}
